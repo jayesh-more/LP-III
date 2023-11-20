c07a4e217bbbe713472e792d81ef0a7cc771ad2d
Jayesh More <65273915+jayesh-more@users.noreply.github.com>
2023-11-20 22:48:49 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,23 @@
+# 1) Write a non-recursive program to calculate Fibonacci numbers and find its step count.
+
+nterms = int(input("Enter number of terms "))
+
+n1, n2 = 0, 1
+count = 0
+
+if nterms <= 0:
+   print("Please enter a positive integer")
+
+elif nterms == 1:
+       print("Fibonacci sequence upto", nterms,":")
+       print(n1)
+
+else:
+    print("Fibonacci sequence:")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
+
---
+++ Fibonacci_Recursive.py
@@ -0,0 +1,11 @@
+# 1) Write a recursive program to calculate Fibonacci numbers and find its step count.
+
+def fibonacci(n):
+    if(n <= 1):
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
+n = int(input("Enter number of terms:"))
+print("Fibonacci sequence:")
+for i in range(n):
+    print(fibonacci(i))
